b292af5cdf24b7396b28e10dd61dc3a78883fa62
Yucheng Song <466309936@qq.com>
2019-07-20 00:30:54 +0800

    Codes of 2 regressions.

--- Homework3/Logical Regression Experiment/main.py
+++ Homework3/Logical Regression Experiment/main.py
@@ -1,20 +1,23 @@
 import numpy as np
 
+# 这是逻辑回归的作业。
 
-def inference(w, b, x):        # inference, test, predict, same thing. Run model after training
+
+def inference(w, b, x_list):        # inference, test, predict, same thing. Run model after training
     """推理函数。
 
         用权值w和b进行推理计算y。
+        这里使用的是乙型函数。
 
         Args:
             w: 模型的权重。
             b:
-            x: 样本值。
+            x_list: 样本值。
 
         Returns:
             返回推理计算的预测值。
     """
-    pred_y = w * x + b
+    pred_y = 1 / (1 + np.e ** ((-1) * (w * x_list + b)))
     return pred_y
 # inference()函数结束
 
@@ -33,9 +36,11 @@ def eval_loss(w, b, x_list, y_list):
         Returns:
             返回推理计算的预测值.
     """
-    loss = 0.5*np.sum((inference(w, b, x_list)-y_list) ** 2)/x_list.size  # loss function
+    h_x_list = inference(w, b, x_list)
+    loss = (-1) / x_list.size / np.sum(y_list*np.log(h_x_list) - (1-y_list)*np.log(1 - h_x_list))   # loss function
 
     return loss
+# eval_loss()
 
 
 def gradient(pred_y, y, x):
@@ -68,45 +73,44 @@ def train(x_list, gt_y_list, batch_size, lr, max_iter):
         print('w:{0}, b:{1}'.format(w, b))
         print('loss is {0}'.format(eval_loss(w, b, x_list, gt_y_list)))
     # 训练结束
+    return w, b
 # train()函数结束。
 
 
 def gen_sample_data():
     """样本生成函数.
 
-            用随机函数生成样本..
+            用随机函数生成二分类样本。
 
             Args:
 
             Returns:
                 返回生成的样本.
     """
-    w = np.random.randint(0, 10) + np.random.random()		# for noise random.random[0, 1)
-    b = np.random.randint(0, 5) + np.random.random()
     num_samples = 100
 
     # 使用NumPy直接生成随机数组
-    x_list = np.random.rand(num_samples)*100
-    y_list = w * x_list + b + np.random.random() * 2 - 1
+    x_list = np.random.rand(num_samples)
+    y_list = np.zeros_like(x_list)
+    y_list[x_list > 0.5] = 1
 
-    return x_list, y_list, w, b
+    return x_list, y_list
+# gen_sample_data()
 
 
 if __name__ == '__main__':
-    x_list, y_list, w, b = gen_sample_data()
+    x_list, y_list = gen_sample_data()
     # 此时获得的w和b则是模型的真实值
     # gen_sample_data()函数测试完成，没有语法错误。
 
     # 模拟一次训练的过程
-    x_list, y_list, w, b = gen_sample_data()
-    lr = 0.0005613
-    # lr = 0.00001
-    max_iter = 20000
-    # max_iter = 200
-    # x_list = np.arange(100)
-    # y_list = np.arange(100)+2
-    train(x_list, y_list, 50, lr, max_iter)
+    x_list, y_list = gen_sample_data()
+    lr = 0.01613
+    max_iter = 30000
+    w, b = train(x_list, y_list, 50, lr, max_iter)
 
     print('Processing finished!')
-    print('w0:{0}, b0:{1}'.format(w, b))
-    # 一元线性回归实验完成！
+
+    print('x:{0}, inference:{1}'.format(0.2, inference(w, b, 0.2)))
+    print('x:{0}, inference:{1}'.format(0.8, inference(w, b, 0.8)))
+    # 逻辑回归实验完成！
